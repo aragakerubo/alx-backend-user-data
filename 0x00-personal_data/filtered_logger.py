0965b77be31b016435fed3ee15cc83d73886c84f
aragakerubo <aragakerubo@gmail.com>
2024-08-31 05:48:37 +0000

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,32 @@ password and the obfuscated email
 """
 import re
 from typing import List
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Constructor method
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the record
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
