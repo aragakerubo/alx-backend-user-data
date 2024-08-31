2c07bf3b77071e20a57d01c5bb4f63754cc44b7c
aragakerubo <aragakerubo@gmail.com>
2024-08-31 23:02:07 +0000

    5. Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,10 +7,10 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Encrypting passwords before storing them in the database
-    
+
     Args:
         password (str): password to encrypt
-        
+
     Returns:
         bytes: encrypted password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
