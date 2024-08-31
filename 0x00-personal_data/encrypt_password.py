d488192531dc2f1b229f2a7ae3295e61cf07d0b2
aragakerubo <aragakerubo@gmail.com>
2024-08-31 23:03:32 +0000

    6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,15 @@ def hash_password(password: str) -> bytes:
     Returns:
         bytes: encrypted password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if a password is valid
+
+    Args:
+        hashed_password (bytes): encrypted password
+        password (str): password to check
+
+    Returns:
+        bool: True if password is valid, False otherwise"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
