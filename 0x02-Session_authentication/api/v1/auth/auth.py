4279fe22a234055a3748ee14e2553fbe2bc70c65
aragakerubo <aragakerubo@gmail.com>
2024-09-05 22:40:24 +0000

    0. Et moi et moi et moi!

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Basic auth
+"""
+from flask import request
+from typing import List, TypeVar
+from os import getenv
+
+
+class Auth:
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+
+        # check if path is in excluded_paths
+        # allowing * at the end of excluded paths
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '*':
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        return None
