2a1da8fc3471b07e9c7eaea2ed7810ee976ad917
aragakerubo <aragakerubo@gmail.com>
2024-09-11 04:14:57 +0000

    7. Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,6 +15,7 @@ def hello() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
     """ POST /users
