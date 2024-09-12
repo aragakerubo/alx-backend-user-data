7287b443704f6de8fe20efd0a7a8bdfbfeb4fdab
aragakerubo <aragakerubo@gmail.com>
2024-09-12 03:40:23 +0000

    11. Log in

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,3 +89,19 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> TypeVar('User'):
+        """Gets a user from a session ID
+
+        Args:
+            session_id (str): session ID
+
+        Returns:
+            TypeVar('User'): User object
+        """
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
